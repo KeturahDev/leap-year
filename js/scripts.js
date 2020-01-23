3c41a8fdc08068252513e7ae16c88ffa08789bab
alondra warren and keturah howard <alondra@>
2020-01-23 13:21:27 -0800

    succeed in second specs goal: return true with %4

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 //business logic
-var leapYear = function() {
+var leapYear = function(year) {
+  if (year % 4 === 0 ) {
+    return true;
+  } else {
     return false;
+  }
+
 }
 
 //user interface
 $(document).ready(function() {
-    $('#leap-year').submit(function(event) {
-        event.preventDefault();
+  $('#leap-year').submit(function(event) {
+  event.preventDefault();
 
-        var year = parseInt($('input#year').val());
+  var year = parseInt($('input#year').val());
 
-        var result = leapYear(year);
-        $('#result').text(result);
-    })
+  var result = leapYear(year);
+  $('#result').text(result);
+  })
 })
